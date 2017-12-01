0a80df5b3f7c1710480236559b5c282e01d6bf0d
Kevin Li Yu <klyu@vt.edu>
2017-12-01 14:09:29 -0500

    Project_Code2.pm is working/Need to create pareto-optimal front/added output files

--- Project_Code2.pm
+++ Project_Code2.pm
@@ -43,24 +43,24 @@ const int visited = 1;
 
 module UAV
 	uav: [0..N] init 0;
-	uav_battery: [-1..battery_levels] init battery_levels;
+	uav_battery: [-10..battery_levels+1] init battery_levels;
 	
-	[] uav = site0 -> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D01-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D01+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D01);
-	[] uav = site0 -> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D02-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D02+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D02);
-	[] uav = site0 -> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D03-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D03+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D03);
-	[] uav = site0 -> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D04-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D04+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D04);
-	[] uav = site1 -> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D12-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D12+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D12);
-	[] uav = site1 -> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D13-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D13+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D13);
-	[] uav = site1 -> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D14-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D14+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D14);
-	[] uav = site2 -> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D12-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D12+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D12);
-	[] uav = site2 -> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D23-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D23+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D23);
-	[] uav = site2 -> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D24-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D24+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D24);
-	[] uav = site3 -> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D13-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D13+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D13);
-	[] uav = site3 -> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D23-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D23+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D23);
-	[] uav = site3 -> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D34-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D34+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D34);
-	[] uav = site4 -> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D14-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D14+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D14);
-	[] uav = site4 -> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D24-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D24+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D24);
-	[] uav = site4 -> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D34-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D34+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D34);
+	[] uav = site0 & uav_battery > -1-> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D01-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D01+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D01);
+	[] uav = site0 & uav_battery > -1-> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D02-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D02+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D02);
+	[] uav = site0 & uav_battery > -1-> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D03-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D03+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D03);
+	[] uav = site0 & uav_battery > -1-> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D04-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D04+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D04);
+	[] uav = site1 & uav_battery > -1-> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D12-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D12+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D12);
+	[] uav = site1 & uav_battery > -1-> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D13-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D13+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D13);
+	[] uav = site1 & uav_battery > -1-> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D14-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D14+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D14);
+	[] uav = site2 & uav_battery > -1-> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D12-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D12+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D12);
+	[] uav = site2 & uav_battery > -1-> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D23-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D23+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D23);
+	[] uav = site2 & uav_battery > -1-> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D24-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D24+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D24);
+	[] uav = site3 & uav_battery > -1-> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D13-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D13+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D13);
+	[] uav = site3 & uav_battery > -1-> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D23-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D23+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D23);
+	[] uav = site3 & uav_battery > -1-> 0.1:(uav' = site4) & (uav_battery' = uav_battery-D34-1) + 0.1:(uav' = site4) & (uav_battery' = uav_battery-D34+1) + 0.8:(uav' = site4) & (uav_battery' = uav_battery-D34);
+	[] uav = site4 & uav_battery > -1-> 0.1:(uav' = site1) & (uav_battery' = uav_battery-D14-1) + 0.1:(uav' = site1) & (uav_battery' = uav_battery-D14+1) + 0.8:(uav' = site1) & (uav_battery' = uav_battery-D14);
+	[] uav = site4 & uav_battery > -1-> 0.1:(uav' = site2) & (uav_battery' = uav_battery-D24-1) + 0.1:(uav' = site2) & (uav_battery' = uav_battery-D24+1) + 0.8:(uav' = site2) & (uav_battery' = uav_battery-D24);
+	[] uav = site4 & uav_battery > -1-> 0.1:(uav' = site3) & (uav_battery' = uav_battery-D34-1) + 0.1:(uav' = site3) & (uav_battery' = uav_battery-D34+1) + 0.8:(uav' = site3) & (uav_battery' = uav_battery-D34);
 
 	// TODO: CHECK TO SEE IF BATTERY LEVEL IS -1 THEN YOU GIVE NEGATIVE INFINITY REWARDS
 	// TODO: NEED TO ADD THE ACTION OF RECHARGIN AND GIVE NEGATIVE 1 REWARDS
@@ -87,17 +87,25 @@ module site_four = site_one[rewards1 = rewards4, s1 = s4, site1 = site4] endmodu
 // reward for all sites visited or not
 rewards "totalRewards"
 	// VISITED A SITE
-	[rewards1] uav_battery > -1: 5; 
-	[rewards2] uav_battery > -1: 5;
-	[rewards3] uav_battery > -1: 5;
-	[rewards4] uav_battery > -1: 5;
+	[rewards1] uav_battery > -1: 50; 
+	[rewards2] uav_battery > -1: 50;
+	[rewards3] uav_battery > -1: 50;
+	[rewards4] uav_battery > -1: 50;
 	
 	// RECHAGE
-	[recharge] true : uav_battery * -1;
+//	[recharge] uav_battery > -1: -1;
 
 	// DEATH OF UAV
-	[] uav_battery < 0 : -100;
+//	[] uav_battery < 0 : -100;
+endrewards
+
+rewards "failures"
+	[] uav_battery < 0 : 1;
+endrewards
+
+rewards "recharge"
+	[recharge] true: 1;
 endrewards
 
 // finish state
-label "done" = (s1=true & s2=true & s3=true & s4=true) | uav_battery=-1;
+label "done" = (s1=true & s2=true & s3=true & s4=true) | uav_battery < 0;
